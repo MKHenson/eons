bc221824a33765baac3c2a9b3cdab86d2b892fec
Mathew Henson <mat@webinate.net>
2019-11-13 22:51:19 +0000

    Added elevation terraces

--- Assets/Scripts/PlanetGeneratorHex/HexMetrics.cs
+++ Assets/Scripts/PlanetGeneratorHex/HexMetrics.cs
@@ -8,6 +8,10 @@ public static class HexMetrics {
   public const float SOLID_FACTOR = 0.75f;
   public const float BLEND_FACTOR = 1f - SOLID_FACTOR;
   public const float ELEVATION_STEP = 5f;
+  public const int TERRACES_PER_SLOPE = 2;
+  public const int TERRACE_STEPS = TERRACES_PER_SLOPE * 2 + 1;
+  public const float HORIZONTAL_TERRACE_STEP_SIZE = 1f / TERRACE_STEPS;
+  public const float VERTICAL_TERRACE_STEP_SIZE = 1f / (TERRACES_PER_SLOPE + 1);
 
   static Vector3[] corners = {
       new Vector3(0f, 0f, OUTER_RADIUS),
@@ -37,5 +41,30 @@ public static class HexMetrics {
 
   public static Vector3 GetBridge(HexDirection direction) {
     return (corners[(int)direction] + corners[(int)direction + 1]) * BLEND_FACTOR;
+  }
+
+  public static HexEdgeType GetEdgeType(int elevation1, int elevation2) {
+    if (elevation1 == elevation2)
+      return HexEdgeType.Flat;
+
+    int delta = elevation2 - elevation1;
+    if (delta == 1 || delta == -1)
+      return HexEdgeType.Slope;
+
+    return HexEdgeType.Cliff;
+  }
+
+  public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step) {
+    float h = step * HexMetrics.HORIZONTAL_TERRACE_STEP_SIZE;
+    a.x += (b.x - a.x) * h;
+    a.z += (b.z - a.z) * h;
+    float v = ((step + 1) / 2) * HexMetrics.VERTICAL_TERRACE_STEP_SIZE;
+    a.y += (b.y - a.y) * v;
+    return a;
+  }
+
+  public static Color TerraceLerp(Color a, Color b, int step) {
+    float h = step * HexMetrics.HORIZONTAL_TERRACE_STEP_SIZE;
+    return Color.Lerp(a, b, h);
   }
 }
